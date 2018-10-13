2da586949e0bc3d488043e75e1a6a319a9464292
TomSssM <ilyashome3@gmail.com>
2018-10-13 16:22:36 +0300

    task 10; end of first part

--- 1) CodeWars.js
+++ 1) CodeWars.js
@@ -410,3 +410,15 @@ function countSmileys(arr) {
 }
 
 //Task 10
+//This is a spin off of my first Kata, you are given a list of characters as a comma separated string.
+//Write a function to return a string containing all except the first and last characters, separated by spaces.
+//If the input string is empty, or the removal of the first and last items would cause the string to be empty, return null value.
+//Arrays are joined by adding a single space between each consecutive array element.
+//Test.assertEquals(array(''), null);
+//Test.assertEquals(array('1'), null);
+//Test.assertEquals(array('1, 3'), null);
+//Test.assertEquals(array('1,2,3'), '2');
+
+function array(arr){
+  return arr.split(",").slice(1,-1).join(" ") || null;
+}
