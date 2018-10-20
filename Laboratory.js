7576d2b532ac4e1ae12c859dd6c01a945d5d6f3c
TomSssM <ilyashome3@gmail.com>
2018-10-20 21:30:24 +0300

    modify two fileses

--- Laboratory.js
+++ Laboratory.js
@@ -1,8 +1,1 @@
-// console.log("Hello\nWorld");
-function prefill(n, v) {
-  if(typeof n === 'boolean' || ~~n != n || +n < 0) throw new TypeError(n + " is invalid");
-  n = +n;
-  let arr = [];
-  arr.length = n;
-  return arr.fill(v);
-}
+console.log("Hello\nWorld");
