187aafa8355799b2d2dfa3ed4930e0e087b3afa3
TomSssM <ilyashome3@gmail.com>
2018-10-27 18:11:14 +0300

    task 16

--- 2) CodeWars.js
+++ 2) CodeWars.js
@@ -196,3 +196,48 @@ function createFunctions(n) {
 }
 
 //Task 16
+// Function composition is a mathematical operation that mainly presents itself in lambda calculus and computability.
+// It is explained well here, but this is my explanation, in simple mathematical notation:
+// f3 = compose( f1 f2 )
+//    Is equivalent to...
+// f3(a) = f1( f2( a ) )
+// Your task is to create a compose function to carry out this task, which will be passed two functions or lambdas.
+// Ruby functions will be passed, and should return, either a proc or a lambda.
+// Remember that the resulting composed function may be passed multiple arguments!
+// compose(f , g)(x)
+// => f( g( x ) )
+// This kata is not available in haskell; that would be too easy!
+
+// const add1 = function(a){return a + 3};
+// const add2 = function(a){return a - 1};
+// const add3 = function(a){return a};
+// const add4 = function(a, b, c){return a + b - c};
+
+// console.log(compose(add1, add2, add3, add4)(2,3,4)); //expected output 3
+
+// My Solution
+
+function compose(...args) {
+  return function(...num) {
+    const len = args.length - 1;
+    let result = args[len](...num);
+    for(let i = len; i > 0; i--) {
+      result = args[i-1](result);
+    }
+    return result;
+  };
+}
+
+// Alternatives ( both work with only 2 functions )
+
+function compose(f, g) {
+  return (...args) => f(g(...args));
+}
+
+function compose(f, g) {
+  return function() {
+    return f(g.apply(null, arguments));
+  };
+}
+
+//Task 17
--- Laboratory.js
+++ Laboratory.js
@@ -1,18 +1,1 @@
-// console.log("Hello\nWorld");
-const add1 = function(a){return a + 3};
-const add2 = function(a){return a - 1};
-const add3 = function(a){return a};
-const add4 = function(a){return a + 7};
-
-function compose(...args) {
-  return function(num) {
-    const len = args.length - 1;
-    let result = args[len](num);
-    for(let i = len; i > 0; i--) {
-      result = args[i-1](result);
-    }
-    return result;
-  };
-}
-
-console.log(compose(add1, add2, add3, add4)(3));
+console.log("Hello\nWorld");
