0ba4e476a76ac21fc9f86afca87720cba4e9282f
TomSssM <ilyashome3@gmail.com>
2018-11-20 13:32:22 +0300

    feat: add task 23

--- 3) CodeWars.js
+++ 3) CodeWars.js
@@ -52,3 +52,58 @@ function construct(Class, ...args) {
 }
 
 //Task 23
+// You are given a complex object that has many deeply nested variables.
+// You don't want to go the usual if obj.property == null route.
+// Create a prototype method that given a nested path, either return the value or undefined.
+
+// var obj = {
+//   person: {
+//     name: 'joe',
+//     history: {
+//       hometown: 'bratislava',
+//       bio: {
+//         funFact: 'I like fishing.'
+//       }
+//     }
+//   }
+// };
+
+// obj.hash('person.name'); // 'joe'
+// obj.hash('person.history.bio'); // { funFact: 'I like fishing.' }
+// obj.hash('person.history.homeStreet'); // undefined
+// obj.hash('person.animal.pet.needNoseAntEater'); // undefined
+
+Object.prototype.hash = function(string) {
+  const hashes = string.split(".");
+  let reference = this;
+  hashes.forEach(element => {
+    if(reference === undefined) {return};
+    reference = reference[element] ? reference[element] : undefined;
+  });
+  return reference;
+}
+
+//alternatives
+//one
+Object.prototype.hash = function(string) {
+  var obj = this;
+  string.split(".").forEach(function(el) { 
+    try {
+      obj = obj[el];
+    }
+    catch(e) { 
+      obj = undefined;
+    }
+  });
+  return obj;
+}
+
+//two
+Object.prototype.hash = function(string) {
+  var arr = string.split('.');
+  return arr.reduce(function(t, v){ 
+    return t ? t[v] : t;
+  }, this);
+}
+
+//Task 24
