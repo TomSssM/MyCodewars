c9eefb4b2fe1d0f109adf9e268df2f2cd77fc46a
TomSssM <ilyashome3@gmail.com>
2018-11-24 19:39:58 +0300

    feat: add task 25

--- 3) CodeWars.js
+++ 3) CodeWars.js
@@ -230,3 +230,51 @@ var Cat = (function () {
   }
   return constr;
 }());
+
+// Task 25
+// You probably know, that in Javascript (and also Ruby) there is no concept of interfaces. There is only a concept of inheritance,
+// but you can't assume that a certain method or property exists, just because it exists in the parent prototype / class.
+// We want to find out, whether a given object fulfils the requirements to implement the "SantaClausable" interface.
+// We need to implement a method which checks for this interface.
+
+// Rules
+// The SantaClausable interface is implemented, if all of the following methods are defined on an object:
+// sayHoHoHo() / say_ho_ho_ho
+// distributeGifts() / distribute_gifts
+// goDownTheChimney() / go_down_the_chimney
+
+// Example
+// var santa = {
+//     sayHoHoHo: function() { console.log('Ho Ho Ho!') },
+//     distributeGifts: function() { console.log('Gifts for all!'); },
+//     goDownTheChimney: function() { console.log('*whoosh*'); }
+// };
+
+// var notSanta = {
+//     sayHoHoHo: function() { console.log('Oink Oink!') }
+//     // no distributeGifts() and no goDownTheChimney()
+// };
+
+// isSantaClausable(santa); // must return TRUE
+// isSantaClausable(notSanta); // must return FALSE
+// Additional Information on this Topic
+// Duck Typing (Wikipedia)
+
+// Info that I wrote myself and that cannot be read as it solves this Kata:
+// Do note that all the properties om the left are functions ( quite possibly data accessors ) so we don't really need
+// to check the properties on the right. But how to determine that the properties on the right really are the setters and getters
+// they are supposed to be ha???
+
+function isSantaClausable(obj) {
+  const allKeys = ["sayHoHoHo","distributeGifts", "goDownTheChimney"];
+  return (typeof obj[allKeys[0]] === "function") && (typeof obj[allKeys[1]] === "function") && (typeof obj[allKeys[2]] === "function");
+}
+
+// Better Implementation
+function isSantaClausable(obj) {
+  return ['sayHoHoHo', 'distributeGifts', 'goDownTheChimney'].every(function(methodName) {
+    return typeof obj[methodName] === 'function';
+  });
+}
+
+// Task 26
