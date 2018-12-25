6347731c2c17b64b5dae4fde6e0cfdab6e8c4aba
TomSssM <ilyashome3@gmail.com>
2018-12-25 22:47:41 +0300

    feat(tutorials): add note upgraded

--- projects-from-the-tutorial/README.md
+++ projects-from-the-tutorial/README.md
@@ -3,9 +3,11 @@
 - [Notification Creator](./notification-creater/notificationCreater.js)
 - [Soccer](./soccer/code.js)
 - [Object to Ul](./object-to-ul)
-  -  [Part One](./object-to-ul/part%20one/Laboratory.js)
-  -  [Part Two](./object-to-ul/part%20two/Laboratory.js)
+  - [Part One](./object-to-ul/part%20one/Laboratory.js)
+  - [Part Two](./object-to-ul/part%20two/Laboratory.js)
 - [Time and Calendar](./calendar-and-time/Laboratory.js)
 - [Sorting a Table](./sorting-a-table/Laboratory.js)
-- [Note](./note/Laboratory.js)
+- Note
+  - [Default](./note/default/Laboratory.js)
+  - [Upgraded](./note/upgraded/Laboratory.js)
 - next
---
+++ projects-from-the-tutorial/note/upgraded/Laboratory.js
@@ -0,0 +1,46 @@
+// we could also check scrollTop / scrollLeft of body and html elements
+// to get the value of scroll but let's enjoy pageYOffset
+// rect.right could be replaced as rect.left + anchor.offsetWidth
+const anchor = document.getElementById('smth');
+
+const positionAt = function(anchor, position = 'top-out', html = '') {
+  const rect = anchor.getBoundingClientRect();
+
+  const element = document.createElement('span');
+  element.classList.add('note');
+  element.textContent = html;
+  document.body.appendChild(element);
+
+  switch(position) {
+    case 'bottom-out':
+    element.style.top = `${rect.bottom + window.pageYOffset}px`;
+    element.style.left = `${rect.left + window.pageXOffset}px`;
+    break;
+    case 'right-out':
+    element.style.left = `${rect.right + window.pageXOffset}px`;
+    element.style.top = `${rect.top + window.pageYOffset}px`;
+    break;
+    case 'top-out':
+    element.style.left = `${rect.left + window.pageXOffset}px`;
+    element.style.top = `${rect.top - element.offsetHeight + window.pageYOffset}px`;
+    break;
+    case 'bottom-in':
+    element.style.top = `${rect.bottom + window.pageYOffset - element.offsetHeight}px`;
+    element.style.left = `${rect.left + window.pageXOffset}px`;
+    break;
+    case 'top-in':
+    element.style.left = `${rect.left + window.pageXOffset}px`;
+    element.style.top = `${rect.top + window.pageYOffset}px`;
+    break;
+    case 'right-in':
+    element.style.left = `${rect.right - element.offsetWidth + window.pageXOffset}px`;
+    element.style.top = `${rect.top + window.pageYOffset}px`;
+  }
+}
+
+positionAt(anchor, 'bottom-out', 'bottom-out');
+positionAt(anchor, 'bottom-in', 'bottom-in');
+positionAt(anchor, 'top-out', 'top-out');
+positionAt(anchor, 'top-in', 'top-in');
+positionAt(anchor, 'right-out', 'right-out');
+positionAt(anchor, 'right-in', 'right-in');
