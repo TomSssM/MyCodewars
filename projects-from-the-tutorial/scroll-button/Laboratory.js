c19dab1ea27ddc5bb8f60417a2fd0821c2f52591
TomSssM <ilyashome3@gmail.com>
2019-01-31 15:24:49 +0300

    fix(tutorial): upgrade scroll-button

--- projects-from-the-tutorial/scroll-button/Laboratory.js
+++ projects-from-the-tutorial/scroll-button/Laboratory.js
@@ -1,7 +1,18 @@
 const button = document.querySelector('#button');
-button.addEventListener('click', function() {
-  const int = setInterval(() => {
-    if(window.pageYOffset === 0) clearInterval(int);
-    window.scrollBy(0, -40);
-  }, 4);
+
+// old crutch solution:
+// button.addEventListener('click', function() {
+//   const int = setInterval(() => {
+//     if(window.pageYOffset === 0) clearInterval(int);
+//     window.scrollBy(0, -40);
+//   }, 4);
+// });
+
+// cool stuff:
+button.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
 });
