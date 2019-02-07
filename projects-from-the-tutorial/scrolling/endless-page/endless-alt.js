73dbafcd90ee29fabb1dcc02678229a97bb2cab2
TomSssM <ilyashome3@gmail.com>
2019-02-08 00:33:24 +0300

    fix(tutorial): add explanation to scrolling/endless-alt.js

--- projects-from-the-tutorial/scrolling/endless-page/endless-alt.js
+++ projects-from-the-tutorial/scrolling/endless-page/endless-alt.js
@@ -4,10 +4,17 @@ function populate() {
   while(true) {
     let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
     if (windowRelativeBottom > document.documentElement.clientHeight + 100) break;
-    document.body.insertAdjacentHTML("beforeend", `<p>Date: ${i++}</p>`);
+    document.body.insertAdjacentHTML("beforeend", `<p>Endless JS ${i++}</p>`);
   }
 }
 
 window.addEventListener('scroll', populate);
-
 populate(); // init document
+
+// windowRelativeBottom is the space in px from the
+// upper edge of the viewport till the document end
+// the populate function is going to keep making paragraphs
+// untill this measure (windowRelativeBottom) exceed the viewport
+// becoming a value of over (viewport height + 100)
+// when we call the populate the first time the windowRelativeBottom is
+// actually even less than viewport so we create the paragraphs
