bd8218403c333f69342e687d71cc234ce885180a
TomSssM <ilyashome3@gmail.com>
2019-03-15 16:03:36 +0300

    feat(tutorial): add graph/clock

---
+++ projects-from-the-tutorial/grap-comp/clock/click-grap.js
@@ -0,0 +1,31 @@
+class Clock {
+  constructor(options) {
+    this.elem = options.elem;
+    // this.start();
+  }
+
+  start() {
+    this.render();
+    this.id = setInterval(() => {
+      this.render();
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.id);
+  }
+
+  prefix(num) {
+    return num < 10 ? `0${num}` : num;
+  }
+
+  render() {
+    const now = new Date();
+    const hour = this.prefix(now.getHours());
+    const min = this.prefix(now.getMinutes());
+    const sec = this.prefix(now.getSeconds());
+    this.elem.querySelector('.hour').textContent = hour;
+    this.elem.querySelector('.min').textContent = min;
+    this.elem.querySelector('.sec').textContent = sec;
+  }
+}
