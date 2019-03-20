c548a2477c545c5e1ef0e8c63f18cc40542a5666
TomSssM <ilyashome3@gmail.com>
2019-03-20 03:02:30 +0300

    feat(snippets): add new features to iterators

--- snippets/iterators/iterators.js
+++ snippets/iterators/iterators.js
@@ -166,3 +166,59 @@ const obj = {
 obj.newProp = 'new property';
 delete obj.b;
 console.log([...obj]);
+
+// Iterating primitives:
+Number.prototype[Symbol.iterator] = function* () {
+  for(let i = 0; i <= this; i++) {
+    yield i;
+  }
+};
+
+console.log(...3);
+
+String.prototype[Symbol.iterator] = function* () {
+  yield 'I';
+  yield 'be';
+  yield 'stringo star';
+};
+
+console.log(...'drummer');
+
+// making a bot with a generator:
+function* botGenerator() {
+  let ans = yield '[bot says:]'+ 'I\'m ready';
+  while(true) {
+    switch (ans) {
+      case '1': {
+        ans = yield '[bot says:]'+ 'I`ve got 1';
+        break;
+      }
+      case '2': {
+        ans = yield '[bot says:]'+ 'I`ve got 2';
+        break;
+      }
+      default: {
+        ans = yield '[bot says:]'+ 'I don`t understand';
+      }
+    }
+  }
+}
+
+const bot = botGenerator();
+console.log(bot.next().value); // I'm ready
+console.log(bot.next('1').value); // I`ve got 1
+console.log(bot.next(23432432432+'asfsdfs').value); // I don`t understand
+console.log(bot.next('2').value); // I`ve got 2
+
+function* botGeneratorVersion2() {
+  let ans = yield '[bot2 says:]' + 'I\'m ready';
+  let wow = yield '[bot2 says:]' + 'redefining';
+  if(ans === '2') yield '[bot2 says:]' + 'answer';
+  if(wow === '2') yield '[bot2 says:]' + 'wow';
+}
+
+const bot2 = botGeneratorVersion2();
+console.log(bot2.next().value); // I'm ready
+console.log(bot2.next('1').value); // redefining
+console.log(bot2.next('2').value); // wow
+console.log(bot2.next(23432432432+'asfsdfs').done); // true
