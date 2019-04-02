5c85c6ff326796f5dcdef1356fd54835959c8279
TomSssM <ilyashome3@gmail.com>
2019-04-03 00:53:12 +0300

    feat(codewars): add task 35

--- 4) Code Wars.js
+++ 4) Code Wars.js
@@ -217,3 +217,54 @@ console.log(eight(minus(three()))); // must return 5
 console.log(six(dividedBy(two()))); // must return 3
 
 // Task 34
+// This kata is designed to test your ability to extend the functionality of built-in classes. 
+// In this case, we want you to extend the built-in Array class with the following methods: 
+// square(), cube(), average(), sum(), even() and odd().
+
+// Explanation:
+
+// - square() must return a copy of the array, containing all values squared
+// - cube() must return a copy of the array, containing all values cubed
+// - average() must return the average of all array values; on an empty array must 
+//             return NaN
+// - sum() must return the sum of all array values
+// - even() must return an array of all even numbers
+// - odd() must return an array of all odd numbers
+// Note: the original array must not be changed in any case!
+
+const numbers = [1, 2, 3, 4, 5];
+
+Array.prototype.square = function() {
+    return this.map(v => v ** 2);
+};
+
+Array.prototype.cube = function() {
+    return this.map(v => v ** 3);
+};
+
+Array.prototype.average = function() {
+    if(!this.length) return NaN;
+    return this.reduce((t,v) => t + v) / this.length;
+};
+
+Array.prototype.sum = function() {
+    if(!this.length) return NaN;
+    return this.reduce((t,v) => t + v);
+};
+
+Array.prototype.even = function() {
+    return this.filter(v => !(v % 2));
+}
+
+Array.prototype.odd = function() {
+    return this.filter(v => v % 2);
+}
+
+console.log(numbers.square());  // must return [1, 4, 9, 16, 25]
+console.log(numbers.cube());    // must return [1, 8, 27, 64, 125]
+console.log(numbers.average()); // must return 3
+console.log(numbers.sum());     // must return 15
+console.log(numbers.even());    // must return [2, 4]
+console.log(numbers.odd());     // must return [1, 3, 5]
+
+// Task 35
