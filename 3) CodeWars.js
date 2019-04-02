7e57e6316639f7c1a38e22e52126640c936e94df
TomSssM <ilyashome3@gmail.com>
2019-04-02 12:59:31 +0300

    feat(codewars): add task 29

--- 3) CodeWars.js
+++ 3) CodeWars.js
@@ -448,3 +448,30 @@ const cipherUpgrecursive = function(strKey, str) {
 cipherUpgrecursive('Something', 'Cool Long String Man'); // Ucap Evvt Ylfurz Tia
 
 // Task 29
+// Write a function which partitions a list of items based on a given predicate.
+// After the partition function is run, the list should be of the form [ F, F, F, T, T, T ] 
+// where the Fs (resp. Ts) are items for which the predicate function returned false (resp. true).
+// NOTE: the partitioning should be stable; in other words: the ordering of the Fs (resp. Ts) 
+// should be preserved relative to each other.
+// For convenience and utility, the partition function should return the boundary index. 
+// In other words: the index of the first T value in items.
+
+// For example:
+// const items = [1, 2, 3, 4, 5, 6];
+// function isEven(n) {return n % 2 == 0}
+// const i = partitionOn(isEven, items);
+
+// items should now be [1, 3, 5, 2, 4, 6]
+// i     should now be 3
+
+function partitionOn(pred, items) {
+  const trueValArr = items.filter(pred);
+  const falseValArr = items.filter(v => !pred(v));
+  const partitioned = [...falseValArr, ...trueValArr];
+  items.forEach((v,i) => {
+    items[i] = partitioned[i];
+  });
+  return falseValArr.length;
+}
+
+// Task 30
