efb8249969b244d9e10f4a75e8d80a0f0ac4a178
TomSssM <ilyashome3@gmail.com>
2019-04-03 15:39:04 +0300

    feat(codewars): add task 36

--- 4) Code Wars.js
+++ 4) Code Wars.js
@@ -373,3 +373,48 @@ console.log(weirdDude instanceof WeirdObjGen); // false
 console.log(weirdDude === Object.prototype); // true
 
 // Task 36
+// In this kata, you must create a digital root function.
+// A digital root is the recursive sum of all the digits in a number. 
+// Given n, take the sum of the digits of n. 
+// If that value has more than one digit, continue reducing in this way until 
+// a single-digit number is produced. This is only applicable to the natural numbers.
+
+// Here's how it works:
+
+// digital_root(16)
+// => 1 + 6
+// => 7
+
+// digital_root(942)
+// => 9 + 4 + 2
+// => 15 ...
+// => 1 + 5
+// => 6
+
+// digital_root(132189)
+// => 1 + 3 + 2 + 1 + 8 + 9
+// => 24 ...
+// => 2 + 4
+// => 6
+
+// digital_root(493193)
+// => 4 + 9 + 3 + 1 + 9 + 3
+// => 29 ...
+// => 2 + 9
+// => 11 ...
+// => 1 + 1
+// => 2
+
+function digital_root(n) {
+    n = String(n);
+    if(n.length === 1) return +n;
+    n = n.split('').reduce((t,v) => t + +v, 0);
+    return digital_root(n);
+}
+
+console.log(digital_root(16)); // 7
+console.log(digital_root(456)); //  6
+console.log(digital_root(942)); // 6
+console.log(digital_root(493193)); // 2
+
+// Task 37
