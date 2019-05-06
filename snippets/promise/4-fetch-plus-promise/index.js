c03d0b3c5e6c47dc079dc18037ff898b32fc4ac7
TomSssM <ilyashome3@gmail.com>
2019-05-06 20:08:03 +0300

    feat(Promise): add fetch-plus-promise

--- snippets/promise/4-fetch-plus-promise/index.js
+++ snippets/promise/4-fetch-plus-promise/index.js
@@ -1,3 +1,49 @@
+// As a rule of thumb, an asynchronous action should always
+// return a promise
 
+document.querySelector('#js-button').addEventListener('click', e => {
+    function loadJson(url) {
+        return fetch(url)
+            .then(response => response.json());
+    }
 
-// As a rule, an asynchronous action should always return a promise.
+    function loadGithubUser(json) {
+        return fetch(`https://api.github.com/users/${json.name}`)
+            .then(response => response.json());
+    }
+
+    function showImage(githubJSON) {
+        return new Promise(res => {
+            const img = document.createElement('img');
+            img.classList.add('github-pic');
+            img.src = githubJSON.avatar_url;
+            img.onload = () => {
+                setTimeout(() => {
+                    res(img);
+                }, 0);
+            };
+            document.body.append(img);
+        });
+    }
+
+    function animateImg(img) {
+        return new Promise(res => {
+            img.classList.add('fade');
+            img.addEventListener('transitionend', () => {
+                res(img);
+            });
+        });
+    }
+
+    function removeImg(img) {
+        return new Promise(res => {
+            img.remove();
+        });
+    }
+
+    loadJson('./user.json')
+        .then(json => loadGithubUser(json))
+        .then(githubJSON => showImage(githubJSON))
+        .then(img => animateImg(img))
+        .then(img => removeImg(img));
+});
