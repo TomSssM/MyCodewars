773db166d50c65874680e9b93223632aa43bd209
TomSssM <ilyashome3@gmail.com>
2019-05-13 00:20:25 +0300

    feat(SSJ): add 1st example to diwn-status

--- snippets/server-side-js/11-down-stat/code-1-download/server.js
+++ snippets/server-side-js/11-down-stat/code-1-download/server.js
@@ -1,29 +1,29 @@
 const http = require('http');
+const {readFile} = require('fs');
+const path = require('path');
 const static = require('node-static');
 const file = new static.Server('.');
 
-http.createServer(function(req, res) {
-    if(req.url === '/upload') {
-        let length = 0;
-        req.on('data', (chunk) => {
-            // don't do anything with the incoming data:
-            length += chunk.length;
-            console.log('data triggered');
-
-            if (length > 50 * 1024 * 1024) {
-                res.statusCode = 413;
-                res.end("File too big");
-            }
-        }).on('end', () => {
-            console.log('start end');
-            // do note that for 1 second the data will be
-            // received but the onload event won't be fired
-            // cause server is doing stuff, that is why it is said
-            // that onprogress event isn't reliable :)
-            const future = Date.now() + 1000;
-            while(Date.now() < future) {}
-            console.log('finish end');
-            res.end('ok');
-        });        
+function accept(req, res) {
+    if (req.url === '/getJSON') {
+        // simulating a very long file:
+        let curr ='';
+        let times = 0;
+        function readTheGoldBug() {
+            readFile(path.join(__dirname, 'theGoldBug.json'), 'utf-8', (err, file) => {
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                times++;
+                curr += file;
+                if(times === 70) {
+                    res.end(curr);
+                } else readTheGoldBug();
+            });
+        }
+        readTheGoldBug();
     } else file.serve(req, res);
-}).listen(5500);
+}
+
+http.createServer(accept).listen(8080);
