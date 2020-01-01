b7210b46e6105a44c86dd930cf341bf1e55ac90f
TomSssM <ilyashome3@gmail.com>
2020-01-01 16:25:17 +0300

    feat(tutorial): finish clipboard revealer

--- projects-from-the-tutorial/clipboard-revealer/index.js
+++ projects-from-the-tutorial/clipboard-revealer/index.js
@@ -1,17 +1,29 @@
 const DEFAULT_TYPE = 'text/plain';
+const MAX_IMAGE_SIZE = 300;
+const MAX_PREVIEW_SIZE = 400;
 const itemKinds = {
     string: 'string',
     file: 'file',
 };
 const contentsElement = document.querySelector('#contents');
-document.addEventListener('paste', (e) => {
-    const { clipboardData } = e;
+
+document.addEventListener('dragover', (e) => {
+    e.preventDefault();
+});
+document.addEventListener('dragstart', (e) => {
+    e.preventDefault();
+});
+document.addEventListener('paste', handleDataArrival);
+document.addEventListener('drop', handleDataArrival);
+
+function handleDataArrival (e) {
+    const eventData = e.clipboardData || e.dataTransfer;
     e.preventDefault();
     let payload = null;
     let kind = null;
     let type = null;
-    if (clipboardData.items) {
-        Array.from(clipboardData.items).forEach((item) => {
+    if (eventData.items) {
+        Array.from(eventData.items).forEach((item) => {
             const { kind, type } = item;
             if (kind === itemKinds.string) {
                 item.getAsString((payload) => {
@@ -30,7 +42,7 @@ document.addEventListener('paste', (e) => {
             }
         });
     } else {
-        payload = clipboardData.getData(DEFAULT_TYPE) || null;
+        payload = eventData.getData(DEFAULT_TYPE) || null;
         if (payload) {
             type = DEFAULT_TYPE;
             kind = itemKinds.string;
@@ -41,12 +53,13 @@ document.addEventListener('paste', (e) => {
             kind,
         });
     }
-});
+}
+
 function renderMessage({ payload, kind, type }) {
     const li = document.createElement('li');
 
-    if (type) {
-        li.innerHTML = `<b>Type</b>: ${type}`;
+    if (type !== null) {
+        li.innerHTML = `<b>Type</b>: ${type || 'unknown'}`;
     } else {
         li.innerHTML = 'Nothing Found';
     }
@@ -56,14 +69,61 @@ function renderMessage({ payload, kind, type }) {
     }
 
     if (kind === itemKinds.file) {
-        li.innerHTML += '<br />Payload: file'; // TODO: here
+        if (isImage(type)) {
+            renderImageFromFile(payload, li);
+        } else {
+            renderFileLink(payload, li);
+        }
     }
 
     contentsElement.append(li);
 }
+
 function sanitize(data) {
     let sanitized = data;
     sanitized = sanitized.replace(/</g, '&lt;');
     sanitized = sanitized.replace(/>/g, '&gt;');
     return sanitized;
+}
+
+function isImage(type) {
+    return /^image\/?/.test(type);
+}
+
+function renderImageFromFile(file, element) {
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+    img.onload = () => {
+        img.width = Math.min(img.width, MAX_IMAGE_SIZE);
+    };
+    element.innerHTML += '<br />';
+    element.append(img);
+}
+
+function renderFileLink(file, element) {
+    const reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = () => {
+        element.innerHTML += '<br />Preview:<br />';
+        const previewElement = document.createElement('pre');
+        const data = reader.result;
+        if (data.length > MAX_PREVIEW_SIZE) {
+            previewElement.textContent = `${data.slice(0, MAX_PREVIEW_SIZE)}...`;
+        } else {
+            previewElement.textContent = data;
+        }
+        element.append(previewElement);
+    };
+
+    reader.onerror = () => {
+        console.log('file read error happened');
+    };
+
+    element.innerHTML += '<br /> Link: ';
+    const a = document.createElement('a');
+    a.download = file.name;
+    a.href = URL.createObjectURL(file);
+    a.textContent = file.name;
+    element.append(a);
 }
