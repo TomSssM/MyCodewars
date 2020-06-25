60e716ad2263557597a699245839ab49cdadebbf
TomSssM <ilyashome3@gmail.com>
2020-06-25 22:01:02 +0300

    feat(cheatsheet): add description to sorter.js

--- cheatsheet/sorter.js
+++ cheatsheet/sorter.js
@@ -1,3 +1,55 @@
+// Implement class `Sorter` with next methods:
+//
+// `constructor` - creates instance of `Sorter` class
+//
+// `add` - add element and store it in any way inside
+//
+// `length` - return the count of current elements, which were added to `Sorter` instance via `add` method
+//
+// `toArray` - return all elements in array. For example:
+// ```js
+//   const sorter = new Sorter();
+//   sorter.add(1);
+//   sorter.add(2);
+//
+//   console.log(sorter.length) // 2
+//   console.log(sorter.toArray()) // [1, 2]
+// ```
+//
+// `sort` - takes indices of already added elements and sorts *only* these elements. For example:
+// ```js
+//   const sorter = new Sorter();
+//   sorter.add(2);
+//   sorter.add(1);
+//
+//   console.log(sorter.length) // 2
+//   console.log(sorter.toArray()) // [2, 1]
+//
+//   sorter.sort([0, 1]);
+//   console.log(sorter.toArray()) // [1, 2]
+// ```
+//
+// `setComparator` - takes `compareFunction` as parameter and use it while sorting elements.
+// You can read about `compareFunction` [here](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/sort).
+// **Sort should work for numbers by default (if compareFunction was not set directly using this method)**
+// For example:
+// ```js
+//   const sorter = new Sorter();
+//   sorter.add(2);
+//   sorter.add(1);
+//
+//   sorter.sort([0, 1]);
+//   console.log(sorter.toArray()) // [1, 2]
+//
+//   sorter.add(3);
+//
+//   const reverseCompareFunction = (left, right) => right - left;
+//   sorter.setComparator(reverseCompareFunction);
+//
+//   sorter.sort([0, 1, 2]);
+//   console.log(sorter.toArray()); // [3, 2, 1]
+// ```
+
 class Sorter {
     static defaultComparator(val1, val2) {
         if (val1 > val2) {
