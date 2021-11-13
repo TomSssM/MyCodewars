1c542e836d07598f0084b98c6a50f082e8ace8be
TomSssM <ilyashome3@gmail.com>
2021-11-13 03:50:44 +0300

    brush up CSS projects

--- CSS/selectors/pseudo-stuff/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('undt').indeterminate = true;
-
-document.getElementById('full').addEventListener('click', function() {
-  this.webkitRequestFullscreen();
-});
-
-document.getElementById('full2').addEventListener('click', function() {
-  this.webkitRequestFullscreen();
-});
